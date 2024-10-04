6ac78021717b68907222d5795a11f7be67d79bdb
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-10-04 01:33:11 +0000

    what about now

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+"""
+0. Pascal's Triangle
+"""
+
 
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-    
-    triangle = []
-    
-    for i in range(n):
-        row = [1] * (i + 1)  # Create a row filled with 1s
-        for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]  # Calculate value based on the previous row
-        triangle.append(row)  # Append the completed row to the triangle
-    
-    return triangle
+    """Create a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal’s triangle of n
+    """
+    res = []
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            C = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                C = C * (i - j) // j
+            res.append(level)
+    return res
