957528a5aba776e1dc0d0c84d2f758b291031a42
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-10-11 05:31:06 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,31 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Solution to Lockboxes problem"""
+
 
 def canUnlockAll(boxes):
-    """
-    Determines if all boxes can be unlocked.
-    :param boxes: List of lists where each list contains keys for other boxes.
-    :return: True if all boxes can be opened, False otherwise.
-    """
-    n = len(boxes)
-    unlocked = [False] * n
-    unlocked[0] = True
-    keys = boxes[0]  # Start with the keys from the first box
-
-    # Keep track of the boxes we can unlock
-    for key in keys:
-        if key < n and not unlocked[key]:
-            unlocked[key] = True
-            keys.extend(boxes[key])  # Add the keys from the newly unlocked box
-
-    return all(unlocked)
-
-
-# Test cases
-boxes1 = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes1))  # True
-
-boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes2))  # True
-
-boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes3))  # False
+    """Determines if all the boxes can be opened"""
+    if (type(boxes) is not list or len(boxes) == 0):
+        return False
+    for k in range(1, len(boxes) - 1):
+        unlocked = False
+        for i in range(len(boxes)):
+            unlocked = k in boxes[i] and k != i
+            if unlocked:
+                break
+        if unlocked is False:
+            return unlocked
+    return True
