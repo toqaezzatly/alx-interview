d8b336006c63f8842d44863308546c420f3d62f5
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-10-25 05:44:50 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,51 @@
 #!/usr/bin/python3
-# This is a script that reads stdin line by line and computes metrics
+"""
+This script reads from stdin line by line and computes the total file size
+and the number of lines by status code. After every 10 lines and/or a keyboard
+interruption (CTRL + C), it prints the statistics from the beginning.
+The input format is `<IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size>` and if the format is not this one, the line
+is skipped. The possible status codes are 200, 301, 400, 401, 403, 404,
+405 and 500. If a status code doesn’t appear or is not an integer,
+nothing is printed for this status code. The status codes are
+printed in ascending order.
+"""
 
 import sys
 
-# We initialize a dictionary to keep track of the status codes and their counts
-status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-cache = {code: 0 for code in status_codes}
-
-# We also initialize variables to keep track of the total file size and the line counter
 total_size = 0
-counter = 0
+status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+status_count = {code: 0 for code in status_codes}
+
+
+def print_stats():
+    """Prints the statistics from the beginning"""
+    print("File size: {}".format(total_size))
+    for code in status_codes:
+        if status_count[code]:
+            print("{}: {}".format(code, status_count[code]))
+
 
 try:
-    # We read from stdin line by line
-    for line in sys.stdin:
-        # We split the line into a list of words
-        line_list = line.split(" ")
-        # If the line does not contain at least 7 words, we skip it
-        if len(line_list) < 7:
+    for count, line in enumerate(sys.stdin, 1):
+        split_line = line.split()
+        if len(split_line) < 2:
             continue
-        # We extract the size and the status code from the line
-        size = line_list[-1]
-        code = line_list[-2]
-        # If the status code is in our cache, we increment its count and add the size to the total size
-        if code in cache:
-            cache[code] += 1
-            total_size += int(size)
-            counter += 1
-
-        # After every 10 lines, we print the statistics
-        if counter == 10:
-            print("File size: {}".format(total_size))
-            for code in sorted(cache.keys()):
-                if cache[code] > 0:
-                    print("{}: {}".format(code, cache[code]))
-            counter = 0
-
-# We handle keyboard interruptions gracefully
+        try:
+            size = int(split_line[-1])
+            total_size += size
+        except ValueError:
+            pass
+        try:
+            status = int(split_line[-2])
+            if status in status_codes:
+                status_count[status] += 1
+        except ValueError:
+            pass
+        if count % 10 == 0:
+            print_stats()
 except KeyboardInterrupt:
-    pass
+    print_stats()
+    raise
 
-# Finally, we print the statistics one last time
-finally:
-    print("File size: {}".format(total_size))
-    for code in sorted(cache.keys()):
-        if cache[code] > 0:
-            print("{}: {}".format(code, cache[code]))
+print_stats()
