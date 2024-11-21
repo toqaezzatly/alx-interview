6d9dfccfdb42dbf2df5bd2122c2897bedfbcf2ba
toqaezzatly <104696786+toqaezzatly@users.noreply.github.com>
2024-11-21 12:06:23 +0200

    2D

---
+++ 0x07-rotate_2d_matrix/0-main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Test 0x07 - Rotate 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(i + 1, len(matrix)):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    return matrix
+
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+
+    rotate_2d_matrix(matrix)
+    print(matrix)
